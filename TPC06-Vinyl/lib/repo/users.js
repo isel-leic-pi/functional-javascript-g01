ad790a6dea6209a4bb210f71b9debd88b012dc1c
Manuel Dias <dias934@gmail.com>
2020-11-13 09:57:55 +0000

    added express and routes

--- TPC06-Vinyl/lib/repo/users.js
+++ TPC06-Vinyl/lib/repo/users.js
@@ -24,28 +24,24 @@ function getUser(username, cb) {
 }
 
 /**
+ * @param {function(Error, User)} cb 
+ */
+function getUsers(cb) {
+    fs.readFile(usersPath, (err, buffer) => {
+        if(err) return cb(err)
+        const arr = JSON.parse(buffer)
+        cb(null, arr)
+    })
+}
+
+/**
  * Add a new User object with given username if it does not exist yet.
  * Returns an Error if that username already exist.
  * @param {String} username 
  * @param {function(Error)}
  */
 function addUser(username, cb) {
-    getUser(username,(err, user) => {
-        if (err) return cb(err);
-        if (user) return cb(new Error(`User ${username} already exists!`))
-        
-        fs.readFile(usersPath, (err, buffer) => {
-            if(err) return cb(err)
-            
-            const arr = JSON.parse(buffer)
-            arr.push({
-                'username': username,
-                'artists': []
-            })
 
-            fs.writeFile(usersPath, JSON.stringify(arr), cb)
-        })
-    })
 }
 
 /**
@@ -58,30 +54,19 @@ function addUser(username, cb) {
  * @param {function(Error, User)} cb 
  */
 function addArtist(username, artist, cb) {
-    fs.readFile(usersPath, (err, buffer)=>{
-        if(err) return cb(err)
-        const arr = JSON.parse(buffer)
-        const selected = arr.filter(user => user.username == username)
-        if(selected.length == 0) return cb(new Error('There is no user ' + username))
-        const newArr=arr.map(user => {
-            if(user.username == username)
-                user.artists.push(artist)
-            return user
-        })
-        fs.writeFile(usersPath, JSON.stringify(newArr), (err)=>{
-            if(err) return cb(err)
-        })
-        cb(null, selected[0])
-    })
+
 }
 
 function init(path) {
     if(path) usersPath = path
-    return {
-        getUser,
-        addArtist,
-        addUser
-    }
+    return API
+}
+const API = {
+    init,
+    getUser,
+    getUsers,
+    addArtist,
+    addUser
 }
 
-module.exports = init
+module.exports = API
